19bc8ce2c0770afbd5f232c4080837ed14868710
douglasbpl <douglas.bpl94@yahoo.com.br>
2021-05-11 17:51:24 -0300

    prática 1 de lógica de programação

---
+++ index.js
@@ -0,0 +1,28 @@
+let name = "Douglas"
+let idade = 27
+let ano = 2021
+const frase = "Meu ano de nascimento é"
+
+console.log(frase, ano - idade)
+
+const souhumano = true
+const nãosouet = false
+const souet = true
+
+
+console.log("Você é humano.", souhumano )
+console.log('Você não é um humano.', nãosouet )
+console.log("Você é um extraterrestre.", souet)
+
+
+let n1 = 439
+let n2 = 81
+let rzqd = Math.sqrt(n2)
+
+console.log("A soma deu", n2 + n1)
+console.log('A subtração deu', n1 - n2)
+console.log("A multiplicação deu", n2 * n1)
+console.log('A divisão deu', n1 / n2)
+console.log("Raiz quadrada de 81 é", rzqd )
+
+
